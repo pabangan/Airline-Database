74e341526f2101bc114e25604cfd6d608b3e2fc2
Brady Anderson <60945821+banderson2112@users.noreply.github.com>
2020-05-03 10:07:18 -0500

    Junk Data + Brady's Stored Procedures
    Creating a bunch of data in strong entities + testing my stored procedures

--- Stored_Procedures_Project.sql
+++ Stored_Procedures_Project.sql
@@ -2,20 +2,80 @@
 
 USE `Airplanes_Project` ;
 
-#### Aircraft and Flights #####
+# FK CHECKS ON
+SET FOREIGN_KEY_CHECKS=1 ;
 
-
-# (1) Insert new Aircraft and assign it to a Flight # w/ Time
+#### Make a bunch of data ####
 
 DELIMITER //
-CREATE PROCEDURE AssignAircraftToFlight(IN Aircraft_Num INT, IN Flight_Num INT, IN CompanyName VARCHAR(45), IN NewStatus VARCHAR(45),
+CREATE PROCEDURE CreateData()
+BEGIN
+
+    DECLARE i INT DEFAULT 001 ;
+    WHILE i <= 1000 DO
+		/* Aircraft Strong Entity */
+        INSERT INTO Aircraft VALUES (i, "Delta",NULL,NULL,NULL,NULL,NULL,NULL, "Available");
+        /* Employee Strong Entity */
+        INSERT INTO Employee VALUES (i, "John","Doe","American");
+        /* Users Strong Entity */
+        INSERT INTO Users VALUES (CONCAT("myemail",i,"@gmail.com"), "Bill","Smith",NULL,'1995-03-10',"Male","123-456-7890","Password!",NULL);
+        SET i = i + 1;        
+	END WHILE ;
+    
+END //
+
+CALL CreateData() ;
+
+
+/* Airports Strong Entity - Make several ones for Big Cities */
+
+INSERT INTO Airports 
+VALUES (1, "Cedar Rapids", "Iowa", NULL), (2, "Orlando", "Florida",NULL), (3, "Dallas", "Texas", NULL), (4, "Minneapolis", "Minnesota", NULL),
+(5, "San Francisco", "California", NULL), (6, "Cincinnati", "Ohio", NULL), (7, "Chicago", "Illinois", NULL),
+(8, "Denver", "Colorado", NULL), (9, "Boston", "Massachusetts",NULL), (10, "Lincoln", "Nebraska", NULL),
+(11, "Detroit", "Michigan", NULL), (12, "Philadelphia", "Pennsylvania", NULL), (13, "Atlanta", "Georgia", NULL) ;
+
+# *EXTRA* For given Aircraft, Populate Seats with ID and Section 
+
+DELIMITER //
+CREATE PROCEDURE Seats_Aircraft(IN Aircraft_Num INT, IN Rows_on_Plane INT)
+BEGIN
+
+    DECLARE i INT DEFAULT 001 ;
+	REPEAT
+        INSERT INTO Seats VALUES (i,Aircraft_Num, "A", i);
+        INSERT INTO Seats VALUES (i + Rows_on_Plane,Aircraft_Num, "B", i);
+        INSERT INTO Seats VALUES (i + 2*Rows_on_Plane,Aircraft_Num, "C", i);
+        INSERT INTO Seats VALUES (i+ 3*Rows_on_Plane,Aircraft_Num, "D", i);
+        INSERT INTO Seats VALUES (i+ 4*Rows_on_Plane,Aircraft_Num, "E", i);
+        INSERT INTO Seats VALUES (i+ 5*Rows_on_Plane,Aircraft_Num, "F", i);
+        SET i = i + 1 ;
+		UNTIL i > Rows_on_Plane
+	END REPEAT ;
+    
+END //
+
+# Fill some in
+CALL Seats_Aircraft(1,25) ;
+CALL Seats_Aircraft(2,25) ;
+CALL Seats_Aircraft(154,40) ;
+CALL Seats_Aircraft(249,35) ;
+
+
+#### Aircraft and Flights - SP #####
+
+
+# (1) Update specific Aircraft  to a Flight # w/ Time - update Aircraft as needed
+
+DELIMITER //
+CREATE PROCEDURE AssignAircraftToFlight(IN Flight_Num INT, IN Aircraft_Num INT,  IN CompanyName VARCHAR(45), IN NewStatus VARCHAR(45),
  IN Dep_Time DATETIME, IN Arr_time DATETIME)
 
 BEGIN
 
-INSERT INTO Aircraft(`idAircraft`,`Company`,`Status`)
-VALUES
-(Aircraft_Num, CompanyName, NewStatus) ;
+UPDATE Aircraft
+SET  `Company` = CompanyName, `Status` = NewStatus
+WHERE `idAircraft` = Aircraft_Num ;
 
 INSERT INTO Flights
 VALUES 
@@ -25,11 +85,36 @@ END //
 
 DELIMITER ;
 
+
 # Test it - make some data
 
 CALL AssignAircraftToFlight(54,249,"American", "Not Available",'2018-07-14 16:30:00','2018-07-14 18:30:00') ;
 
 CALL AssignAircraftToFlight(16,154,"Delta", "Not Available",'2019-07-19 16:0:00','2019-07-19 19:30:00') ;
+
+
+# Extra SP - Flight Destinations
+
+DELIMITER //
+CREATE PROCEDURE InsertFlightDestinations(IN Flight_Num INT, IN AirportID INT,
+IN Location VARCHAR(45))
+
+BEGIN
+
+INSERT INTO `Flight Destinations`
+VALUES 
+(Flight_Num,AirportID, Location) ;
+
+END //
+
+DELIMITER ;
+
+# Test it
+CALL InsertFlightDestinations(16,1,"Departing") ;
+CALL InsertFlightDestinations(16,2,"Arriving") ;
+
+CALL InsertFlightDestinations(54,6,"Departing") ;
+CALL InsertFlightDestinations(54,13,"Arriving") ;
 
 
 # (2)  Aircraft Table - Grab first N records of table
@@ -50,7 +135,7 @@ DELIMITER ;
 
 # Check it
 
-CALL FirstNAircrafts(1) ;
+CALL FirstNAircrafts(14) ;
 
 
 # (3) Flights Table - Get all flights arriving between two specific times
@@ -120,6 +205,7 @@ DELIMITER ;
 
 CALL UpdateUser("testing123@gmail.com", "StrongerPassword!") ;
 
+
 #### Your Trips ####
 
 # (1) Book a trip for a User
@@ -139,8 +225,6 @@ END //
 DELIMITER ;
 
 # Check - add data 
-
-# Q? Is there a function which when you create data will make an attribute with when it was created?
 
 CALL BookFlight("testing123@gmail.com", 16, '2019-05-12 19:03:21') ;
 CALL BookFlight("sql_lover21@gmail.com", 54, '2018-02-12 10:01:01') ;
@@ -165,93 +249,3 @@ DELIMITER ;
 # Check
 
 CALL GetTrips("testing123@gmail.com") ;
-
-
--- BEGINING OF PHIL'S STORED PROCEDURES ---------------------------------------------------------------------------------------------------
--- news about vacations----------------------------------------------------
-DELIMITER //
-CREATE PROCEDURE spRetrieveNewsByAirportID(IN input_idAirports INT)
-BEGIN
-	SELECT News
-    FROM Airports
-    WHERE idAirports = input_idAirports;
-    
-END //
-
-DELIMITER ;
-
-# Test it - make some data
-INSERT INTO Airports(`idAirports`,`City`,`State`, `News`)
-VALUES
-(1, 'Chicago', 'Illinois',"News about O'Hare International Airport") ;
-
-CALL spRetrieveNewsByAirportID(1) ;
-
--- Contact Us ------------------------------------------------------------------ Data not stored b/c contact info stored in users
-
--- About Us ------------------------------------------------------------------ No data stored for About Us. Will be front end.
-
--- Advanced Search ---------------------------------------------------------------------------------
-DELIMITER //
-CREATE PROCEDURE spSearchFlightsByCityDestination(IN input_FlightCityDestination VARCHAR(45))
-BEGIN
-	SELECT *
-    FROM Flights
-    WHERE idFlights IN (SELECT Flights_idFlights from Flight Destinations where Airports_idAirports IN (SELECT idAirports from Airports where City = input_FlightCityDestination));
-END //
-
-DELIMITER ;
-# Test it - make some data
-INSERT INTO Airports(`idAirports`,`City`,`State`, `News`) VALUES (1, 'Chicago', 'Illinois',"News about O'Hare International Airport") ;
-INSERT INTO Aircraft(`idAircraft`,`Company`,`Status`) VALUES (16, "Not Available", "Not Available") ;
-INSERT INTO Flights VALUES (16,154,"Delta", "Not Available",'2019-07-19 16:0:00','2019-07-19 19:30:00');
-
-CALL spSearchFlightsByCityDestination('Chicago') ;
-
-------------------------------------------------------------------------------------------------
-
-DELIMITER //
-CREATE PROCEDURE spSearchFlightsByCityandTime(IN input_FlightCityDestination VARCHAR(45), IN input_BeginningTime DATETIME, IN input_EndTime DATETIME) 
-BEGIN
-	SELECT *
-    FROM Flights
-    
-    
-    WHERE 
-		(`Departure Time` <= input_BeginningTime) -- beginning from this condition and
-		AND (`Arrival Time` >= input_EndTime) -- ending before this condition
-		AND (idFlights IN (SELECT Flights_idFlights from Flight Destinations where Airports_idAirports IN (SELECT idAirports from Airports where City = input_FlightCityDestination))); -- City condition
-END //
-
-DELIMITER ;
-# Test it - make some data
-INSERT INTO Airports(`idAirports`,`City`,`State`, `News`) VALUES (1, 'Chicago', 'Illinois',"News about O'Hare International Airport") ;
-INSERT INTO Aircraft(`idAircraft`,`Company`,`Status`) VALUES (16, "Not Available", "Not Available") ;
-INSERT INTO Flights VALUES (16,154,"Delta", "Not Available",'2019-07-19 16:0:00','2019-07-19 19:30:00');
-
-CALL spSearchFlightsByCityandTime('Chicago', GETDATE(), '2050-07-19 19:30:00') ;
-
-
--- Trip cancellation or change ------------------------------------------------------------------ 
-DELIMITER //
-CREATE PROCEDURE spDelFlightFromUser(IN input_userID INT, IN input_flightID)
-BEGIN
-	DELETE 
-    FROM table_name 
-    WHERE condition;
-END //
-
-DELIMITER ;
-# Test it - make some data
-INSERT INTO Airports(`idAirports`,`City`,`State`, `News`) VALUES (1, 'Chicago', 'Illinois',"News about O'Hare International Airport") ;
-INSERT INTO Aircraft(`idAircraft`,`Company`,`Status`) VALUES (16, "Not Available", "Not Available") ;
-INSERT INTO Flights(`idFlights`, `Aircraft_idAircraft`, `Departure Time`, `Arrival TIme`) 
-	VALUES (16,154,"Delta", "Not Available",'2019-07-19 16:0:00','2019-07-19 19:30:00');
-INSERT INTO Users(`Primary Email`, `First Name`, `Last Name`, `Middle Name`, `Date of Birth`, `Gender`, `Primary Phone #`, `Password`, `Known Travelor #`) 
-	VALUES ('test@test.com', 'TestFirst', 'TestLast', 'TestMiddle', '1996-02-08 16:0:00', 'Male', '1234567899', 'password', 1);
-INSERT INTO Bookings(`Flights_idFlights`, `Users_Primary Email`, `Time Ordered`) VALUES (16, 'test@test.com', '2019-07-19 16:0:00');
-INSERT INTO Ticket(`Seats_idSeats`, `Bookings_Flights_idFlights`, `Bookings_Users_Primary Email`, `Amenities`, `Cost`) VALUES (); 
-
-CALL spDelFlightFromUser(1,16) ;
-
--- END OF PHIL'S STORED PROCEDURES -------------------------------------------------------------------------------------------------------
